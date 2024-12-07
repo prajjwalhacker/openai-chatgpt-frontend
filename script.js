846321eeadd9e203e5736709a929a779890ba151
prajjwalsoni123 <prajjwalsoni123@gmail.com>
2024-12-07 16:00:29 +0530

    chat script changes

--- script.js
+++ script.js
@@ -1,21 +1,35 @@
-import bot from './assets/bot.svg';
-import user from './assets/user.svg';
 
 const form = document.querySelector('form');
 const chatContainer = document.querySelector('#chat_container');
 
-let loadInterval;
+function apiForAnswer() {
 
-const loading = '';   
-
-const appendInLoading = () => {
-     setInterval(() => {
-       if (loading.length === 3) {
-          loading = '.';    
-       }
-       else {
-        loading += '.';
-       } 
-     }, 500);
 }
 
+
+const generateRandomId = () => {
+    const str = 'abcdefsfsdkjdfdsfjdsf12493249203390@E@@(#@(#(@$(@';
+    let id = '';
+    for (let i =0; i < str.length; i++) {
+       id += str[Math.ceil(Math.random() * str.length)];
+    }
+    return id;
+ }
+
+function appendUserMessage(id, message) {
+    const userDiv = document.createElement('div');
+    userDiv.classList.add('user-chat');
+    userDiv.setAttribute('id', `user-chat-${id}`);
+    userDiv.textContent = message;
+    chatContainer.appendChild(userDiv);
+}
+
+const buttonContaier = document.getElementById('send-button');
+const textAreaDiv = document.getElementById('input-box');
+
+buttonContaier.addEventListener('click', () => {
+
+    const id = generateRandomId();
+    const message = textAreaDiv.value;
+    appendUserMessage(id, message);
+});
